09fca57bad1ca9f43bfc583fa6b5d94a600ac8be
Pavel Ovchinnikov <ops-23@ya.ru>
2023-02-21 18:33:30 +0300

    Homework 6 is done. Added the solution tasks 43

--- Seminar6Task43/Program.cs
+++ Seminar6Task43/Program.cs
@@ -1,57 +1,58 @@
 Console.Clear();
-// Console.WriteLine("Привет! Это задание выполнено Павлом Овчинниковым при обучении основам языка С#");
-// Console.WriteLine(@"
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
-// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
-// значения b1, k1, b2 и k2 задаются пользователем.
-// Например: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-// (от Голикова А.С.) * Найдите площадь треугольника образованного пересечением 3 прямых.
-// ");
+Console.WriteLine("Привет! Это задание выполнено Павлом Овчинниковым при обучении основам языка С#");
+Console.WriteLine(@"
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+Например: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+(от Голикова А.С.) * Найдите площадь треугольника образованного пересечением 3 прямых.
+");
 
 // Console.WriteLine();
 
 /// Метод ввода вещественного числа
-// double ReadData(string msg)
-// {
-//     Console.Write(msg);
-//     // double read_Data = double.Parse(Console.ReadLine() ?? "0");
-//     string? read_string_Data = Console.ReadLine() ?? "0";
-//     double read_Data;
-//     while (!double.TryParse(read_string_Data, out read_Data))
-//     {
+double ReadData(string msg)
+{
+    Console.Write(msg);
+    // double read_Data = double.Parse(Console.ReadLine() ?? "0");
+    string? read_string_Data = Console.ReadLine() ?? "0";
+    double read_Data;
+    while (!double.TryParse(read_string_Data, out read_Data))
+    {
 
-//         Console.ForegroundColor = ConsoleColor.Red;
-//         Console.Write("Ошибка! Повторите ввод: ");
-//         Console.ResetColor();
-//         read_string_Data = Console.ReadLine() ?? "0";
-//     }
-//     return read_Data;
-// }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Ошибка! Повторите ввод: ");
+        Console.ResetColor();
+        read_string_Data = Console.ReadLine() ?? "0";
+    }
+    return read_Data;
+}
 
 
 // /// Метод задания 3х прямых линий на плоскости
-// double[,] EnterLine(double[,] c_l)
-// {
-//     /// Ввод с клавиатуры коэффициентов функций прямой линии на плоскости
-//     Console.WriteLine("Задайте функцию прямой линии Аx + Вy + С = 0");
-//     Console.WriteLine("Запятая - разделитель целой и дробной части вещественного числа");
+double[,] EnterLine(double[,] c_l)
+{
+    /// Ввод с клавиатуры коэффициентов функций прямой линии на плоскости
+    Console.WriteLine("Задайте функцию прямой линии Аx + Вy + С = 0");
+    Console.WriteLine("Запятая - разделитель целой и дробной части вещественного числа");
 
-//     for (int i = 0; i < 3; i++)
-//     {
-//         Console.WriteLine("Введите по одному через Enter значения коэф-в А,В и С для прямой №" + (i + 1));
-//         for (int j = 0; j < 3; j++)
-//         {
-//             c_l[i, j] = ReadData("Введите " + (j + 1) + "й " + "коэффициент : ");
-//         }
-//         Console.WriteLine($"Задана {i + 1}-я прямая: {c_l[i, 0]}x + {c_l[i, 1]}y + {c_l[i, 2]} = 0");
-//         Console.WriteLine();
-//     }
-//     return c_l;
-// }
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine("Введите по одному через Enter значения коэф-в А,В и С для прямой №" + (i + 1));
+        for (int j = 0; j < 3; j++)
+        {
+            c_l[i, j] = ReadData("Введите " + (j + 1) + "й " + "коэффициент : ");
+        }
+        Console.WriteLine($"Задана {i + 1}-я прямая: {c_l[i, 0]}x + {c_l[i, 1]}y + {c_l[i, 2]} = 0");
+        Console.WriteLine();
+    }
+    return c_l;
+}
 
 //  Метод вычисления точек пересечения  (координата вершины треугольника) 
 double[,] CrossTwoLines(double[,] c_l, double[,] triangle_vertex)
 {
+    int k = 0;
     for (int i = 0; i < c_l.GetLength(0); i++)
     {
         for (int j = i + 1; j < c_l.GetLength(1); j++)
@@ -65,51 +66,41 @@ double[,] CrossTwoLines(double[,] c_l, double[,] triangle_vertex)
             }
             else
             { // Вычисление точки пересечения
-
-                // X:= (C1 * B2 - C2 * B1) / (B1 * A2 - B2 * A1);
-                // Y:= (C2 * A1 - C1 * A2) / (B1 * A2 - B2 * A1);
-
-                // Console.WriteLine($" для {i} и {j} : {c_l[i, j]}, {c_l[i+1, j+1]}, {c_l[i+2, j+2]} ");
-
-                triangle_vertex[i, 0] = (c_l[i, 2] * c_l[j, 1] - c_l[j, 2] * c_l[i, 1]) / znl;
-                triangle_vertex[i, 1] = (c_l[j, 2] * c_l[i, 0] - c_l[i, 2] * c_l[j, 0]) / znl;
-
-                Console.WriteLine("Прямые пересекаются в точке: " + triangle_vertex[i, 0] + ", " + triangle_vertex[i, 1]);
+                triangle_vertex[k, 0] = (c_l[i, 2] * c_l[j, 1] - c_l[j, 2] * c_l[i, 1]) / znl;
+                triangle_vertex[k, 1] = (c_l[j, 2] * c_l[i, 0] - c_l[i, 2] * c_l[j, 0]) / znl;
+                k++;
             }
-            // Console.WriteLine($"Для i={i}, j={j}, Х={triangle_vertex[i, 0]}, Y={triangle_vertex[i, 1]}");
         }
     }
-    return triangle_vertex;  // ЗДЕСЬ ГДЕ-ТО ОШИБКА ! ВОЗВРАЩАЕТ МАТРИЦУ, не ту, что получили в цикле! А с одной пустой сторокой
+    return triangle_vertex;
 }
 
-// /// Метод Вычисления расстояние между точками в 2D пространстве
-// // double CalcLengthLine(double[,] arr)
-// // {
+/// Метод Вычисления расстояние между точками в 2D пространстве
+double[] CalcSide(double[,] arr)
+{
+    double[] sides = new double[3]; // Массив из 3х сторон треугольника
 
-// //     for (int i = 0; i < 3; i++)
-// //     {
-// //         for (int j = 0; j < 3; j++)
-// //         {
-// //     LengthLine = Math.Sqrt(Math.Pow((arr[] - x2), 2) + Math.Pow((y1 - y2), 2));
+    sides[0] = Math.Sqrt(Math.Pow((arr[0, 0] - arr[1, 0]), 2) + Math.Pow((arr[0, 1] - arr[1, 1]), 2));
+    sides[1] = Math.Sqrt(Math.Pow((arr[0, 0] - arr[2, 0]), 2) + Math.Pow((arr[0, 1] - arr[2, 1]), 2));
+    sides[2] = Math.Sqrt(Math.Pow((arr[1, 0] - arr[2, 0]), 2) + Math.Pow((arr[1, 1] - arr[2, 1]), 2));
 
-// //     return LengthLine;
-// // }
+    return sides;
+}
 
+/// Метод Вычисления периметра треугольника
+double TrianglePerimeter(double[] sides)
+{
+    double p = (sides[0] + sides[1] + sides[2]);
+    return p;
+}
 
-// /// Метод Вычисления периметра треугольника
-// // var TrianglePerimeter(sideA, sideB, sideC)
-// // {
-// //     p = (sideA + sideB + sideC);
-// //     return;
-// // }
-
-// /// Метод Вычисления площади треугольника
-// // var TriangleArea(sideA, sideB, sideC)
-// // {
-// //     var p = TrianglePerimeter(sideA, sideB, sideC);
-// //     s = Math.Sqrt(p / 2 * (p / 2 - A) * (p / 2 - B) * (p / 2 - C));
-// //     return s;
-// // }
+/// Метод Вычисления площади треугольника
+double TriangleArea(double[] sides)
+{
+    double p = (sides[0] + sides[1] + sides[2]); // Вычисления периметра треугольника
+    double triangle_area = Math.Sqrt(p / 2 * (p / 2 - sides[0]) * (p / 2 - sides[1]) * (p / 2 - sides[2]));
+    return triangle_area;
+}
 
 void Print2DArray(double[,] arr)
 {
@@ -123,14 +114,18 @@ void Print2DArray(double[,] arr)
     }
 }
 
-
 /// Main - Блок решения задачи
 
-double[,] c_l = { { 1, 3, 8 }, { -1, -1, 4 }, { 0.5, -1, 4 } };
-// double[,] c_l = new double[3, 3]; // c_l constants_line Массив 3х прямых линий на плоскости
+// double[,] c_l = { { 1, 3, 8 }, { -1, -1, 4 }, { 0.5, -1, 4 } };
+// double[,] c_l = { { 1, 0, 0 }, { 0, 1, 0 }, { 4, 3, -12 } }; // Египетский треугольник
 
+// определяем переменные - массивы коэф-в прямых линий, точки их пересечения и длины сторон треугольника
+double[,] c_l = new double[3, 3]; // Массив 3х прямых линий на плоскости c_l от constants_of_lines
 double[,] triangle_vertex = new double[3, 2]; // Массив 3х точек пересечения прямых - вершин треугольника
-                                              // EnterLine(c_l);
+double[] sides = new double[3]; // Массив из 3х сторон треугольника
+
+EnterLine(c_l);  // Вводим коэф. линий с клавиатуры
+
 Console.WriteLine("Массив 3х прямых линий на плоскости");
 Print2DArray(c_l);
 Console.WriteLine();
@@ -140,10 +135,15 @@ Console.WriteLine();
 
 Console.WriteLine("Массив 3х точек пересечения прямых - вершин треугольника");
 Print2DArray(triangle_vertex);
-Console.WriteLine("ГДЕ-ТО ОШИБКА в методе CrossTwoLines! return ВОЗВРАЩАЕТ не ту МАТРИЦУ, что получили в цикле! А с одной пустой сторокой");
+Console.WriteLine();
 
+sides = CalcSide(triangle_vertex);
+Console.WriteLine($"Получаем треугольник со сторонами: {sides[0]} , {sides[1]} , {sides[2]}");
+Console.WriteLine();
 
-// // Console.WriteLine("Получаем треугольник со сторонами длинной: " + sideA + ", " + sideB + ", " + sideC);
-// // Console.WriteLine("Периметр треугольника равен: {0}", TrianglePerimeter());
-// // Console.WriteLine("Площадь треугольника равна: {0}", TriangleArea());
-Console.WriteLine(" ");
+double triangle_area = TriangleArea(sides);
+Console.WriteLine($"Площадь треугольника равна: {triangle_area}");
+double p = TrianglePerimeter(sides);
+Console.WriteLine($"Периметр треугольника равен: {p}");
+Console.WriteLine();
+Console.WriteLine(" Ура! Хитрость обманула здравый смысл");
